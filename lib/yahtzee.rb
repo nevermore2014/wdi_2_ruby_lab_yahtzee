3cdf9a8d908a53243c76f13bb35ba0668c437047
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-16 16:18:15 -0500

    Create #initialize method with class YahtzeeRoll. Pass test for valid num of dice

---
+++ lib/yahtzee.rb
@@ -0,0 +1,13 @@
+class YahtzeeRoll
+	attr_accessor :dice1, :dice2, :dice3, :dice4, :dice5
+
+
+	def initialize(*dice)
+		raise 'invalid number of dice' if dice.length != 5
+		@dice1 = dice[0]
+		@dice2 = dice[1]
+		@dice3 = dice[2]
+		@dice4 = dice[3]
+		@dice5 = dice[4]
+	end
+end
--- spec/yahtzee_spec.rb
+++ spec/yahtzee_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/yahtzee'
 describe YahtzeeRoll do
 	describe "#initialize" do
 		it "should have five args" do
-			expect{ YahtzeeRoll.new(1, 4, 2, 3, 7) }.to raise_error 'invalid number of dices'
+			expect{ YahtzeeRoll.new(1, 4, 2, 3) }.to raise_error 'invalid number of dice'
 		end
 	end
 end
