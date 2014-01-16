a087946768de2e8c23716bad525dfc895c0fb73e
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-16 17:03:02 -0500

    :three tested and passed

--- lib/yahtzee.rb
+++ lib/yahtzee.rb
@@ -14,10 +14,14 @@ class YahtzeeRoll
 	def scoring(category)
 		sum = 0
 		case category
-		when :pair
-			@dice.each do |i|
-				@dice.count(i) == 2 ? sum += i : sum += 0
-		end
+			when :pair
+				@dice.each do |i|
+					sum += i if @dice.count(i) == 2
+				end
+			when :three
+				@dice.each do |i|
+					sum += i if @dice.count(i) == 3
+				end
 		end
 		sum
 	end
